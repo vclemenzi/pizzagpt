d624246570ac09acd9026b04e09981550f7e5dbb
Thebigbot <77632836+vclemenzi@users.noreply.github.com>
2023-04-01 10:24:53 +0200

    feat: added login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+
 const Home: NextPage = () => {
+  const [key, setKey] = useState("");
+
+  useEffect(() => {
+    const key = localStorage.getItem("key");
+
+    if (!key) {
+      window.location.href = "/login";
+    } else {
+      setKey(key);
+    }
+  }, []);
+
   return (
     <>
       <Head>
         <title>PizzaGPT</title>
-        <meta name="description" content="Pizza GPT: A chatgpt client for the italian users because the main was banned :(" /> 
+        <meta
+          name="description"
+          content="Pizza GPT: A chatgpt client for the italian users because the main was banned :("
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
---
+++ src/pages/login.tsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+
+function Login() {
+  const [key, setKey] = useState("");
+
+  useEffect(() => {
+    const key = localStorage.getItem("key");
+
+    if (key) {
+        window.location.href = "/";
+    }
+    }, []);
+
+  const handleLogin = () => {
+    localStorage.setItem("key", key);
+    window.location.href = "/";
+  };
+
+  return (
+    <>
+      <Head>
+        <title>PizzaGPT</title>
+      </Head>
+      <div className="-mt-56 flex min-h-screen flex-col items-center justify-center px-14 py-2 text-center">
+        <div className="rounded-lg bg-white p-10 shadow-xl">
+          <h1 className="mb-5 text-2xl">Insert your key</h1>
+          <p className="mb-5 text-gray-500">
+            Insert your OpenAI API Key to use PizzaGPT
+          </p>
+          <input
+            className="w-full rounded border border-gray-300 p-2"
+            id="key"
+            type="password"
+            placeholder="OpenAI API Key"
+            value={key}
+            onChange={(e) => setKey(e.target.value)}
+          />
+
+          <button
+            className="mt-4 w-full rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={handleLogin}
+          >
+            Start using PizzaGPT
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
