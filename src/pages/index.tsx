e89985552546a9be736c640a530b8619f959883c
Thebigbot <77632836+vclemenzi@users.noreply.github.com>
2023-04-01 09:59:37 +0200

    feat: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>PizzaGPT</title>
+        <meta name="description" content="Pizza GPT: A chatgpt client for the italian users because the main was banned :(" /> 
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  );
+};
+
+export default Home;
